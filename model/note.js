43b8581eb43df36985e54f591168b29a691a51d2
li_shuai <506507881@qq.com>
2017-11-07 01:01:18 +0800

    实现便利贴的增删改功能

--- model/note.js
+++ model/note.js
@@ -1,7 +1,52 @@
+var path = require('path')
+var Sequelize = require('sequelize');  
+
 var sequelize = new Sequelize(undefined, undefined, undefined, {
   host: 'localhost',
   dialect: 'sqlite',
-
   // 仅 SQLite 适用
-  storage: 'path/to/database.sqlite'
+  storage: path.join(__dirname,'../database/database.sqlite') 
 });
+/*测试note.js是否成功
+sequelize
+.authenticate()
+.then(() => {
+  console.log('Connection has been established successfully.');
+})
+.catch(err => {
+  console.error('Unable to connect to the database:', err);
+});
+*/
+
+// 1 hello 1111111 2344124
+
+//定义一个表结构
+var Note = sequelize.define('note', {
+  text: {
+    type: Sequelize.STRING
+  }
+});
+
+/*
+//假设这个表不存在，就去创建他;
+//如果存在，什么也不管。
+Note.sync().then(function(){
+  //创建一条数据
+  Note.create({text: 'hello world'})
+}).then(function(){
+  // 去查找，查找到展示数据
+  Note.findAll({raw:true}).then(function(notes){
+    console.log(notes)
+  })
+})
+*/
+
+
+/*
+//raw:true 为原始数据;where:{id:2}查找id为2的数据
+  Note.findAll({raw:true,where:{id:2}}).then(function(notes){
+  console.log(notes)
+})
+*/
+
+module.exports.Note=Note;
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var Note = require('../model/note')
+var Note = require('../model/note').Note;
 
 //   1. 获取所有 note:GET  /api/notes   req:{}    res:{status: 0,data:[{},{}]} {status: 1,
 //   errorMsg: '失败的原因'}
@@ -11,17 +11,33 @@ var Note = require('../model/note')
 
 /* GET users listing. */
 router.get('/notes', function(req, res, next) {
-  var data = note.getAll()
-  res.send({status:0,data:data})
+  console.log('/notes...')
+  // var data = note.getAll()
+  Note.findAll({raw:true}).then(function(notes){
+    console.log(notes)
+    res.send({status:0,data:notes})
+  })
+});
 router.post('/notes/add', function(req, res, next) {
   var note = req.body.note;
+  Note.create({text:note}).then(function(){
+    res.send({status:0})
+  }).catch(function(){
+    res.send({status:1,errorMsg:'数据库出错'})
+  })
   console.log('add...',note)
 });
 router.post('/notes/edit', function(req, res, next) {
-  res.send('respond with a resource');
+  Note.update({text: req.body.note},{where:{id:req.body.id}}).then(function(){
+    console.log(arguments)
+    res.send({status:0})
+  })
 });
+
 router.post('/notes/delete', function(req, res, next) {
-  res.send('respond with a resource');
+  Note.destroy({where:{id:req.body.id}}).then(function(){
+    res.send({status:0})
+  })
 });
 
 module.exports = router;
